260d23b7f095ae984c9651f81030025cc232fc74
ssssylassss <125253203+ssssylassss@users.noreply.github.com>
2024-02-12 14:22:54 -0600

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/footer";
 import Social from "@/components/social";
 import Companies from "@/components/companies";
 import Newsletter from "@/components/newsletter";
+import Pricing from "@/components/pricing";
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
       <Social />
       <Companies />
       <Newsletter />
+      <Pricing />
       <Footer />
     </main>
   );
